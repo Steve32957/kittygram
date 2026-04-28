f3c9d44da82061b0b9845ff655391e522e7c531b
KyleSantano80094 <orhantahmazov520@gmail.com>
2026-04-28 21:22:12 +0500

    View-classes

--- cats/views.py
+++ cats/views.py
@@ -1,34 +1,41 @@
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+# Обновлённый views.py
+from rest_framework import generics
 
 from .models import Cat
 from .serializers import CatSerializer
 
 
-@api_view(['GET', 'POST'])  # Разрешены только POST- и GET-запросы
-def cat_list(request):
-    # В случае POST-запроса добавим список записей в БД
-    if request.method == 'POST':
-        serializer = CatSerializer(data=request.data, many=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    # В случае GET-запроса возвращаем список всех котиков
-    cats = Cat.objects.all()
-    serializer = CatSerializer(cats, many=True)
-    return Response(serializer.data)
+# высокоуровневые view-классы
+class CatList(generics.ListCreateAPIView):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
 
 
-@api_view(['GET', 'POST'])  # Применили декоратор и указали разрешённые методы
-def hello(request):
-    # По задумке, в ответ на POST-запрос нужно вернуть JSON с теми данными,
-    # которые получены в запросе.
-    # Для этого в объект Response() передаём словарь request.data.
-    if request.method == 'POST':
-        return Response({'message': 'Получены данные', 'data': request.data})
+class CatDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
 
-    # В ответ на GET-запрос нужно вернуть JSON
-    # Он тоже будет создан из словаря, переданного в Response()
-    return Response({'message': 'Это был GET-запрос!'})
+# Обновлённый views.py
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
+# from rest_framework import status
+
+# from .models import Cat
+# from .serializers import CatSerializer
+
+
+# низкоуровневый view-класс
+# class APICat(APIView):
+#     def get(self, request):
+#         cats = Cat.objects.all()
+#         serializer = CatSerializer(cats, many=True)
+#         return Response(serializer.data)
+
+#     def post(self, request):
+#         serializer = CatSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(
+#                   serializer.errors, status=status.HTTP_400_BAD_REQUEST
+# )
